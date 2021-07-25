1b109b16402a052b0cc4ef9249ac1a6ff10edbc8
byeungoo <dksekfldks65@naver.com>
2021-07-26 00:37:34 +0900

    타임리프 기본 기능 학습 완료

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -126,4 +126,17 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
 }
